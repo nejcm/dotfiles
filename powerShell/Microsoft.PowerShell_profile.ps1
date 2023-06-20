ddf1ed3e71a5669c96d07e5dcf72d34751584884
Nejc <nmursi2@gmail.com>
2023-06-20 13:36:06 +0800

    feat: updated files

--- powerShell/Microsoft.PowerShell_profile.ps1
+++ powerShell/Microsoft.PowerShell_profile.ps1
@@ -1,48 +1,45 @@
-<# Remove-Alias gc -Force
-Remove-Alias gco -Force
-Remove-Alias ga -Force
-Remove-Alias gb -Force
-Remove-Alias gba -Force
-Remove-Alias gbd -Force
-Remove-Alias gcp -Force
-Remove-Alias gd -Force
-Remove-Alias gds -Force
-Remove-Alias grs -Force
-Remove-Alias gst -Force
-Remove-Alias gu -Force
-Remove-Alias gpr -Force
-Remove-Alias ff -Force
-Remove-Alias grd -Force
-Remove-Alias gbb -Force
-Remove-Alias gl -Force
-Remove-Alias gla -Force
-Remove-Alias grc -Force
-Remove-Alias gra -Force
-Remove-Alias gcan -Force
-Remove-Alias gpf -Force #>
-
 # GIT ALIASES
-function gc{git commit}
-function gco{git checkout}
-function ga{git add}
-function gb{git branch}
-function gba{git branch --all}
-function gbd{git branch -D}
-function gcp{git cherry-pick}
-function gd{git diff -w}
-function gds{git diff -w --staged}
-function grs{git restore --staged}
-function gst{git rev-parse --git-dir > /dev/null 2>&1 && git status || exa}
-function gu{git reset --soft HEAD~1}
-function gpr{git remote prune origin}
-function ff{gpr && git pull --ff-only}
-function grd{git fetch origin && git rebase origin/master}
-function gbb{git-switchbranch}
-function gl{git log --graph --format=format:'%C(bold blue)%h%C(reset) - %C(white)%s%C(reset) %C(green)%an %ar %C(reset) %C(bold magenta)%d%C(reset)'}
-function gla{git log --all --graph --format=format:'%C(bold blue)%h%C(reset) - %C(white)%s%C(reset) %C(bold magenta)%d%C(reset)'}
-function grc{git rebase --continue}
-function gra{git rebase --abort}
-function gcan{gc --amend --no-edit}
-function gpf{git push --force-with-lease}
+function ff   {gpr && git pull --ff-only}
+function ga   {git add $args}
+function gb   {git branch $args}
+function gba  {git branch --all $args}
+function gbb  {git-switchbranch $args}
+function gbd  {git branch -D $args}
+function gc   {git commit $args}
+function gcm  {git commit -m $args}
+function gca  {git commit -a $args}
+function gcan {gc --amend --no-edit $args}
+function gco  {git checkout $args}
+function gcp  {git cherry-pick $args}
+function gd   {git diff -w $args}
+function gds  {git diff -w --staged $args}
+function gl   {git log --graph --format=format:'%C(bold blue)%h%C(reset) - %C(white)%s%C(reset) %C(green)%an %ar %C(reset) %C(bold magenta)%d%C(reset)'}
+function gla  {git log --all --graph --format=format:'%C(bold blue)%h%C(reset) - %C(white)%s%C(reset) %C(bold magenta)%d%C(reset)'}
+function gpl  {git pull $args}
+function gp   {git push $args}
+function gpnv {git push --no-verify $args}
+function gpf  {git push --force-with-lease $args}
+function gpr  {git remote prune origin}
+function gra  {git rebase --abort $args}
+function grc  {git rebase --continue $args}
+function grd  {git fetch origin && git rebase origin/master}
+function grs  {git restore --staged $args}
+function gst  {git rev-parse --git-dir > /dev/null 2>&1 && git status || exa $args}
+function gu   {git reset --soft HEAD~1}
+
+function cua  {choco upgrade all -y}
+
+Remove-Alias gal -Force
+Remove-Alias gc -Force
+Remove-Alias gcm -Force
+Remove-Alias gi -Force
+Remove-Alias gl -Force
+Remove-Alias gm -Force
+Remove-Alias gmo -Force
+Remove-Alias gp -Force
+Remove-Alias gps -Force
+Remove-Alias gpv -Force
+Remove-Alias gu -Force
+Remove-Alias gv -Force
 
 Invoke-Expression (&starship init powershell)
